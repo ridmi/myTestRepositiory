95b27882fdb903132c437d8c26491049b9b43f0b
Dmitry Rizhikov <d.rizhikov@seeburger.de>
2015-03-04 13:39:59 +0100

    sequential commit 2

---
+++ GitGit2/src/main/java/com/see/git/test/FetureForBlaBla1.java
@@ -0,0 +1,23 @@
+/*
+ * FetureForBlaBla1.java
+ *
+ * created at 04.03.2015 by d.rizhikov <d.rizhikov@seeburger.de>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package com.see.git.test;
+
+
+public class FetureForBlaBla1
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
+
+
