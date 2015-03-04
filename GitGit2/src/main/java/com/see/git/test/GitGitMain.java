ad87c75ea2d630104d4d9dd9be8a4d3321e01a7e
Dmitry Rizhikov <d.rizhikov@seeburger.de>
2015-03-04 13:32:33 +0100

    initial commit

---
+++ GitGit2/src/main/java/com/see/git/test/GitGitMain.java
@@ -0,0 +1,23 @@
+/*
+ * GitGitMain.java
+ *
+ * created at 04.03.2015 by d.rizhikov <d.rizhikov@seeburger.de>
+ * 
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package com.see.git.test;
+
+
+public class GitGitMain
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
+
+
